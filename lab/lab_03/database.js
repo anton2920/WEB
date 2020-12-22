cc8f8ded16fd80e1904e22a9c032952c599bdfa3
anton2920 <anton2920@gmail.com>
2020-12-22 13:44:20 +0300

    PC: lab_03 (almost done)

---
+++ lab/lab_03/database.js
@@ -0,0 +1,119 @@
+
+const guitars = [
+	{
+		brand: "Fender",
+		name: "Fender Stratocaster American Standard Sunburst",
+		year: "1954",
+		body: "Stratocaster",
+		country: "the U.S.A",
+		type: "Single coil",
+		img: 'img/1.png'
+	},
+	{
+		brand: "Fender",
+		name: "Fender Player Telecaster Butterscotch Blonde",  
+		year: "1950",
+		body: "Telecaster",
+		country: "the U.S.A",
+		type: "Single Coil",
+		img: 'img/2.png'
+	},
+	{
+		brand: "Gibson",
+		name: "Gibson Les Paul Custom White",
+		year: "1952",
+		body: "Les Paul",
+		country: "the U.S.A",
+		type: "Humbacker",
+		img: 'img/4.png'
+	},
+	{
+		brand: "Gibson",
+		name: "Gibson SG Special",
+		year: "1961",
+		body: "SG",
+		country: "the U.S.A",
+		type: "Single Coil",
+		img: 'img/5.png'
+	},
+	{
+		brand: "Gibson",
+		name: "Gibson Explorer Lzzy Hale Custom White",
+		year: "1958",
+		body: "Explorer",
+		country: "the U.S.A",
+		type: "Humbacker",
+		img: 'img/6.png'
+	},
+	{
+		brand: "Ibanez",
+		name: "Ibanez Prestige SAS32EXFM",
+		year: "2008",
+		body: "Stratocaster",
+		country: "Japan",
+		type: "Humbacker",
+		img: 'img/7.png'
+	},
+	{
+		brand: "Jackson",
+		name: "Jackson JS32 Dinky DKA-M MN SW Snow White",
+		year: "1986",
+		body: "Stratocaster",
+		country: "the U.S.A",
+		type: "Humbacker",
+		img: 'img/8.png'
+	},
+	{
+		brand: "Dean",
+		name: "Dean V Dave Mustaine Guitar: Rust In Peace",
+		year: "1977",
+		body: "Flying V",
+		country: "the U.S.A",
+		type: "Humbacker",
+		img: 'img/9.png'
+	},
+	{
+		brand: "Gibson",
+		name: "Gibson Flying V",
+		year: "1958",
+		body: "Flying V",
+		country: "the U.S.A",
+		type: "Humbacker",
+		img: 'img/10.png'
+	},
+	{
+		brand: "Epiphone",
+		name: "Epiphone Les Paul Standard",
+		year: "1988",
+		body: "Les Paul",
+		country: "the U.S.A",
+		type: "Humbacker",
+		img: 'img/11.png'
+	},
+	{
+		brand: "ESP",
+		name: "ESP EX James Hetfield Snakebyte Snow White",
+		year: "1985",
+		body: "Explorer",
+		country: "Japan",
+		type: "Humbacker",
+		img: 'img/12.png'
+	},
+	{
+		brand: "ESP LTD",
+		name: "ESP LTD Telecaster Ron Wood Black",
+		year: "1995",
+		body: "Stratocaster",
+		country: "Japan",
+		type: "Single coil",
+		img: 'img/13.png'
+	}
+]
+
+const properties = {
+'brand': "Наименование бренда",
+'country': "Страна происхождения бренда",
+'year': "Год выпуска первой модели",
+'body': "Форма деки",
+'type': "Тип звукоснимателей"
+};
---
+++ lab/lab_03/sc.js
@@ -0,0 +1,154 @@
+function printGuitar(arGuitars, selector)
+{
+    /* Initializing variables */
+    const startTemplate = '<h4> Каталог: </h4>';
+    const lileTemplate = '<div class="item"><div><img src="{{img}}"></div><ul><li>{{name}}</li><li>{{year}}</li><li>{{body}}</li><li>{{country}}</li><li>{{type}}</li><ul></div>';
+    const endTemplate = '</table>';
+    
+    let output = startTemplate;
+    
+    /* Main part */
+    for (let item of arGuitars) {
+        let tmpLine;
+        tmpLine = lileTemplate.replace('{{name}}', item.name);
+        tmpLine = tmpLine.replace('{{year}}',     item.year);
+        tmpLine = tmpLine.replace('{{img}}',     item.img);
+        tmpLine = tmpLine.replace('{{body}}',      item.body);
+        tmpLine = tmpLine.replace('{{country}}',   item.country);
+        tmpLine = tmpLine.replace('{{type}}',   item.type);
+        output += tmpLine;
+    };
+    
+    output += endTemplate;
+    $(selector).html(output);
+}
+
+function printFilters(arProperties, selector)
+{
+    /* Initializing variables */
+    const startTemplate = '<br>{{name}}<br>';
+    const lileTemplate  = '<label><input type="checkbox" id="{{name}}" name="{{prop}}" value="{{name}}">{{name}}</label><br>';
+    const endTemplate   = '';
+    let output = '';
+
+    /* Main part */
+    for (let prop in arProperties) {
+        let tmpLine = startTemplate.replace('{{name}}', arProperties[prop]);
+        let vals    = [];
+
+        for (let guitar of guitars) {
+            if (!vals.includes(guitar[prop])) {
+                vals.push(guitar[prop]);
+            }
+        }
+    
+        vals.sort();
+        vals.forEach(function(item) {
+            tmpLine += lileTemplate.replace("{{prop}}", prop ).replaceAll("{{name}}",item);
+        });
+        
+        output += tmpLine;
+    
+    }
+    
+    output += endTemplate;
+    $(selector).html(output);
+}
+
+function readCurFilters(properties)
+{
+    /* Initializing variables */
+    let result = [];
+
+    /* Main part */
+    for (let prop in properties) {
+        let searchIDs = $("#filters input[name='"+prop+"']:checkbox:checked").map(function(){
+            return $(this).val();
+        }).get();
+    
+        result[prop] = searchIDs;
+    }
+    
+    /* Returning value */
+    return result;
+}
+
+function applyFilters(data, filter, properties)
+{
+    /* Initializing variables */
+    let result = [];
+
+    /* Main part */
+    for (let guitar of data) {
+        let ok = true;
+        for (let prop in properties) {
+            if (!filter[prop].length) {
+                continue;
+            }
+
+            if (filter[prop].indexOf(guitar[prop]) == -1) {
+                ok = false;
+            }
+        }
+        
+        if (ok) {
+            result.push(guitar);
+        };
+    }
+
+    /* Returning value */
+    return result;
+}
+
+function getEmptyFilters(properties)
+{
+    /* Initializing variables */
+    let filters = [];
+
+    /* Main part */
+    for (let prop in properties) {
+        let searchIDs = $("#filters input[name='"+prop+"']:not(:checked)").map(function() {
+        return $(this).val();
+        }).get();
+    
+        filters[prop] = searchIDs;
+    }
+
+    /* Returning value */
+    return filters;
+}
+
+
+function closeEmptyCheckBox(curFilter, filters, guitars, properties)
+{
+    /* Main part */
+    for (let prop in properties) {
+        for (let filter in filters[prop]) {
+            curFilter[prop].push(filters[prop][filter]);
+            if (applyFilters(guitars, curFilter, properties).length === 0) {
+                document.getElementById(filters[prop][filter]).setAttribute('disabled', 'true');
+            } else {
+                document.getElementById(filters[prop][filter]).removeAttribute('disabled');
+            }
+            curFilter[prop].pop();
+        }
+    }
+}
+
+
+$(document).ready(function()
+{
+    /* Main part */
+    printGuitar(guitars, '#elements');
+    printFilters(properties, '#filters');
+
+    $('#filters input').change(function()
+    {
+        let curFilter      = readCurFilters(properties);
+        let filtredGuitars = applyFilters(guitars, curFilter, properties);
+        let unchecked      = getEmptyFilters(properties);
+        closeEmptyCheckBox(curFilter, unchecked,guitars, properties);
+
+        printGuitar( filtredGuitars, '#elements' );
+    });
+});
